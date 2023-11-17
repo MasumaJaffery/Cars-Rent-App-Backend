21509a6152eb6b1fa6704cb36acc02d63ed2efce
MasumaJaffery <masumajaffery1@gmail.com>
2023-11-18 04:30:06 +0500

    Implement feature X
    Co-authored-by: Cancelei <glauber.bannwart@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_17_222145) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_17_232726) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,17 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_17_222145) do
     t.string "added_by"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "car_id", null: false
+    t.date "date"
+    t.string "city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["car_id"], name: "index_reservations_on_car_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
   create_table "users", id: :serial, force: :cascade do |t|
@@ -38,4 +49,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_17_222145) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reservations", "cars"
+  add_foreign_key "reservations", "users"
 end
