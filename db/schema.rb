c4fd30f28cbb3f6bd7e50d001a0212c1ae5a74af
Najibullahjafari <najib2020202020@gmail.com>
2023-11-22 14:13:52 +0430

    Fix linter

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,55 +12,55 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_20_181026) do
+ActiveRecord::Schema[7.1].define(version: 20_231_120_181_026) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "cars", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.string "category"
-    t.string "added_by"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'cars', force: :cascade do |t|
+    t.string 'name'
+    t.string 'description'
+    t.string 'category'
+    t.string 'added_by'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "items", force: :cascade do |t|
-    t.string "name", null: false
-    t.decimal "price", null: false
-    t.string "city", null: false
-    t.text "description", null: false
-    t.string "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'items', force: :cascade do |t|
+    t.string 'name', null: false
+    t.decimal 'price', null: false
+    t.string 'city', null: false
+    t.text 'description', null: false
+    t.string 'image'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "reservations", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "car_id", null: false
-    t.date "date"
-    t.string "city"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["car_id"], name: "index_reservations_on_car_id"
-    t.index ["user_id"], name: "index_reservations_on_user_id"
+  create_table 'reservations', force: :cascade do |t|
+    t.bigint 'user_id', null: false
+    t.bigint 'car_id', null: false
+    t.date 'date'
+    t.string 'city'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['car_id'], name: 'index_reservations_on_car_id'
+    t.index ['user_id'], name: 'index_reservations_on_user_id'
   end
 
-  create_table "users", id: :serial, force: :cascade do |t|
-    t.string "name", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "jti", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["jti"], name: "index_users_on_jti", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  create_table 'users', id: :serial, force: :cascade do |t|
+    t.string 'name', null: false
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'jti', null: false
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['jti'], name: 'index_users_on_jti', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
 
-  add_foreign_key "reservations", "cars"
-  add_foreign_key "reservations", "users"
+  add_foreign_key 'reservations', 'cars'
+  add_foreign_key 'reservations', 'users'
 end
