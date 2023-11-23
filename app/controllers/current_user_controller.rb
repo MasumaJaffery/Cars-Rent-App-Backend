158022e534964a4e7f0bfaead58188cbd425308e
Syeda Masuma Fatima <masumajaffery1@gmail.com>
2023-11-24 00:20:37 +0500

    Revert "Feat/reserve logic"

--- app/controllers/current_user_controller.rb
+++ app/controllers/current_user_controller.rb
@@ -1,21 +1,31 @@
 class CurrentUserController < ApplicationController
-  before_action :authenticate_user!
-
-  def show
-    render json: current_user, status: :ok
+  def index
+    @users = User.all
+    render json: @users, status: :ok
   end
 
   def update
-    if current_user.update(user_params)
-      render json: current_user, status: :ok
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      render json: @user, status: :ok
     else
-      render json: { errors: current_user.errors.full_messages }, status: :unprocessable_entity
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      render json: { status: 'SUCCESS', message: 'Deleted user', data: @user }, status: :ok
+    else
+      render json: { status: 'ERROR', message: 'User not deleted', data: @user.errors },
+             status: :unprocessable_entity
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email) # Adjust attributes as needed
+    params.require(:user).permit(:name, :id, :email)
   end
 end
